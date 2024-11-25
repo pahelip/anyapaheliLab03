f36944ade42ea90d9b41e92a644ddad0ad39071f
pahelip <pahelipatel@gmail.com>
2024-11-24 21:07:10 -0500

    Add files via upload

---
+++ info.py
@@ -0,0 +1,76 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.jpeg"
+about_me = "I am Tony Stark - genius, billionare, playboy, philanthropist."
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/spongebob-squarepants-44b857281/"
+my_github_url = "https://github.com/spongebob-squarepants"
+my_email_address = "tonystark@avengers.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Electrical Engineering',
+    'Institution': 'Massachusetts Institute of Technology',
+    'Location': 'Cambridge, MA',
+    'Graduation Date': 'At age 17',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["ENGG 101", "ROBO 201", "PHYS 305", "AI 400"], 
+    "names":["Intro to Engineering", "Advanced Robotics", "Applied Physics", "Artificial Intelligence"], 
+    "semester_taken":["Completed in a week", "1st", "High School", "1dt"],
+    "skills":["Built my first robot at age 6", "Mastered replusor technology and energy physics", "Created the original Iron Man suit in a cave", "Able to drive a Formula 1"],
+    }
+experience_data = {
+    "CEO of Stark Industries": (["- Designed the Iron Man suits", "- Created sustainable arc reactor technology for energy", "- Boosted company revenue by creating defense technology"],"Images/explode.jpeg"),
+    "Inventor":(["- Created J.A.R.V.I.S., an AI assistant", "- Developed the nano-suit and space-time navigation devices", "- Worked with SHIELD to design weaponry and defense systems"],"Images/work.jpeg"),
+    "Avenger":(["- Saved the world multiple times", "- Led the Avengers in the Battle of New York and Infinity War"],"Images/avenger.jpeg")
+}
+
+projects_data = {
+    "Iron Man Suit": "Created the famous Iron Man suit in a cave and built upgraded new models with different skills since",
+    "Time Travel": "Figured out how to acquire the Infinity Stones from different points in time by using the quantum realm"
+}
+
+programming_data = {
+    "Genius ": 99,
+    "Billionaire ": 100,
+    "Philanthropy ": 50
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Genius ": "🧠",
+    "Billionaire ": "💰",
+    "Philanthropy ": "❤️"
+}
+spoken_icons = {"French ": "🇫🇷",
+    "English ": "🇬🇧",
+    "Japanese ":"🇯🇵",
+    "Russian ":"🇷🇺"      
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English ": " Fluent",
+    "French ": " Fluent",
+    "Russian ": " Fluent",
+    "Japanese ": " Fluent"
+}
+leadership_data = {
+    "Leader of the Avengers": (["- Planned missions to protect Earth", "- United a team with diverse abilities to work together"],"Images/suit.jpeg"),
+    "CEO of Stark Industries": (["- Helped with US defense strategies and futuristic technology designs"],"Images/hand.jpeg")
+}
+activity_data={
+    "Maria Stark Foundation": ["- Created to honor my mother", 
+            "- Donated profits from Stark Industries to support the foundation"]
+}
