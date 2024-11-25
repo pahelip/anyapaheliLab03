98c6ed775654481114988ad140d242f8fe222975
pahelip <pahelipatel@gmail.com>
2024-11-24 20:49:00 -0500

    Add files via upload

---
+++ Home_Page.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab03")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Team 22, Web Development - Section B")
+st.subheader("Anya Chaganti, Paheli Patel")
+st.image("Images/homepage.jpeg")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:""")
+
+st.write("""1. **Tony Stark's Portfolio**: An online portfolio of Tony Stark's professional and heroic experience.""")
+st.write("""2. **Peter Parker's Portfolio**: An online portfolio of Peter Parker's professional and heroic experience.""")
+st.write("""3. **Page**: Description""")
+st.write("""4. **Page**: Description""")
+
+
+
+
